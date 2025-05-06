f4bb14e8f514b247fd3908edd283ebcc6f94c596
Mathis Lauriol-Torcq <mathis.lauriol-torcq@etu.univ-amu.fr>
2025-05-06 13:53:59 +0200

    Exercice 8 fini

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,29 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+
+
 
 public class LoginControl extends GridPane {
 
+    @FXML
+    private TextField user;
+
+    @FXML
+    private PasswordField pwd;
+
+    @FXML
     private void okClicked() {
+        System.out.println(user.getText());
+        System.out.println("*".repeat(pwd.getText().length()));
     }
 
+    @FXML
     private void cancelClicked() {
+        user.setText("");
+        pwd.setText("");
     }
 }
